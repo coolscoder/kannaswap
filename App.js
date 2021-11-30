53dd6f4afa5636f3d8e7dc25d09bf7741c8411f8
Koichi Noda <koichi.p.noda@gmail.com>
2021-11-30 17:56:56 +0900

    feat: clean up code base

--- App.js
+++ App.js
@@ -8,10 +8,12 @@ import {
 } from 'react-native'
 import { Button, Input, Overlay } from 'react-native-elements'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const App = () => {
   const [overlay, setOverlay] = useState(false)
   const [password, setPassword] = useState()
+  const [secure, setSecure] = useState(true)
 
   return (
     <SafeAreaProvider>
@@ -23,13 +25,29 @@ const App = () => {
             <Text style={styles.description}>Input your password for security reason so we can transfer the money</Text>
             <Input
               label="password"
+              labelStyle={styles.inputLabel}
               placeholder='Type Password'
-              secureTextEntry={true}
-              leftIcon={{ type: 'font-awesome', name: 'lock' }}
-              rightIcon={{ type: 'font-awesome', name: 'eye-slash' }}
+              secureTextEntry={secure}
+              leftIcon={
+                <Icon
+                  name='lock'
+                  style={styles.icon}
+                  size={24}
+                  color='black'
+                />
+              }            
+              rightIcon={
+                <Icon
+                  name={secure ? 'eye-slash' : 'eye'}
+                  style={styles.icon}
+                  size={24}
+                  color='black'
+                  onPress={() => setSecure(!secure)}
+                />
+              }  
               onChangeText={e => setPassword(e)}
             />
-            <Button title="Send" />
+            <Button style={styles.sendBtn} title="Send" />
           </View>
         </Overlay>
       </View>
@@ -55,14 +73,28 @@ const styles = StyleSheet.create({
     marginBottom: 0,
     padding: 20,
   },
+  inputLabel: {
+    marginLeft: 30,
+    fontSize: 15,
+    fontWeight: 'normal',
+    color: '#C5C5C5'
+  },
   title: {
     fontSize: 18,
     marginBottom: 15,
+    marginLeft: 20,
   },
   description: {
     fontSize: 15,
     color: '#938989',
     marginBottom: 30,
+    marginHorizontal: 20,
+  },
+  icon: {
+    marginHorizontal: 10,
+  },
+  sendBtn: {
+    backgroundColor: '#fff',
   }
 })
 
